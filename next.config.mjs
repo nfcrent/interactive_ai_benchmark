5491782a57232eaa0c1382a8bcd62a22a1dec1b7
nfcrent <info@nfcrent.com>
2025-08-01 07:02:45 +0000

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,24 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        // Apply to all routes
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *;"
+          }
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
